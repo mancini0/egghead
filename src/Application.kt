add1d220d8ff7d74c8a6e99a1e6d16751b199b80
ronmexico <mancini0@gmail.com>
2019-06-10 21:24:46 -0500

    initial commit

---
+++ src/Application.kt
@@ -0,0 +1,62 @@
+
+
+import com.google.cloud.texttospeech.v1beta1.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.Apache
+import io.ktor.client.features.json.*
+import io.ktor.client.request.*
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.gson
+
+
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused")
+fun Application.module() {
+    val client = HttpClient(Apache) {
+        install(JsonFeature) {
+            serializer = GsonSerializer()
+        }
+        engine{
+            followRedirects = true
+        }
+    }
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+        }
+    }
+
+    routing {
+         get("/")  {
+            val wikiNugget = client.get<WikiNugget> {
+                url("https://en.wikipedia.org/api/rest_v1/page/random/summary")
+                contentType(ContentType.Application.Json)
+            }
+             val ttsClient = TextToSpeechClient.create()
+
+             val input = SynthesisInput.newBuilder()
+                 .setText(wikiNugget.extract)
+                 .build()
+
+             val voice = VoiceSelectionParams.newBuilder()
+                 .setLanguageCode("en-US")
+                 .setSsmlGender(SsmlVoiceGender.MALE)
+                 .build()
+             val audioConfig = AudioConfig.newBuilder()
+                 .setAudioEncoding(AudioEncoding.OGG_OPUS)
+                 .build()
+
+
+             call.respondBytes(ContentType.Audio.OGG, HttpStatusCode.OK, suspend {ttsClient.synthesizeSpeech(input,voice,audioConfig).audioContent.toByteArray()})
+        }
+    }
+}
+
+class WikiNugget(val title: String, val extract: String, val pageid: Long)
